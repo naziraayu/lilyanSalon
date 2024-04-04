c1ae10e08abf78fe315ed5ba374a66420ca57ddc
naziraayu <naziraayu2003@gmail.com>
2024-04-04 23:38:45 +0700

    first

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    public function login(Request $request) {
+        $this->validate($request, [
+            'username' => 'required|string',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $loginType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'password';
+
+        $login = [
+            $loginType => $request->username,
+            'password' => $request->password,
+        ];
+
+        if (auth()->attempt($login)) {
+            return redirect()->route('template');
+        }
+        return redirect()->route('login')->with(['error' => 'Email/Password salah.']);
+    }
+
+    // public function authenticate(Request $request): RedirectResponse
+    // {
+    //     $credentials = $request->validate([
+    //         'email' => ['required', 'email'],
+    //         'password' => ['required'],
+    //     ]);
+ 
+    //     if (Auth::attempt($credentials)) {
+    //         $request->session()->regenerate();
+ 
+    //         return redirect()->intended('admin');
+    //     }
+ 
+    //     return back()->withErrors([
+    //         'email' => 'The provided credentials do not match our records.',
+    //     ])->onlyInput('email');
+    // }
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\LandingController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/index', [LandingController::class, 'index'])->name('landing');
+
+Route::get('/admin', [AdminController::class, 'admin'])->name('template');
+
+Route::get('/forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])
+    ->name('password.request');
+
+// Rute untuk mengirim email reset kata sandi
+Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])
+    ->name('password.email');
+
+// Rute untuk menampilkan formulir reset kata sandi
+Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])
+    ->name('password.reset');
+
+// Rute untuk proses reset kata sandi
+Route::post('/reset-password', [ResetPasswordController::class, 'reset'])
+    ->name('password.update');
+
+Route::get('/data_menu', [MenuController::class, 'index'])->name('data_menu');
+Route::get('/tambah_data_menu', [MenuController::class,'create'])->name('tambah_data_menu');
+Route::post('/tambah_data_menu', [MenuController::class,'store'])->name('tambah_data_menu');
+Route::get('/edit_data_menu/{id}', [MenuController::class,'edit'])->name('edit_data_menu');
+Route::post('/update_data_menu/{id}', [MenuController::class,'update']);
+Route::post('/hapus_data_menu/{id}', [MenuController::class,'destroy'])->name('hapus_data_menu');
+// Route::get('/admin',[MenuController::class,'index'])->name('admin');
