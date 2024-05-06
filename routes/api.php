375067bd0a2ecb00c52f3c13f8a320fb79e656b9
gabrielhabibi <124557052+gabrielhabibi@users.noreply.github.com>
2024-05-06 14:27:31 +0700

    Gabriel
    rest API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/auth/Register', [AuthController::class,'register'])->name('register');
+Route::post('/auth/Login', [AuthController::class,'login'])->name('login');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('auth/Me', [AuthController::class, 'me']);
+    Route::post('auth/Logout', [AuthController::class, 'logout']);
 });
